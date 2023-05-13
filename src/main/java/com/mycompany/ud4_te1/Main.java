2654435cbdcc3f34c235c221623fc09c09d519d4
Mikel <Mikel@DESKTOP-VF0LKT2>
2023-05-13 19:18:16 +0200

    Version 2 creamos metodo operativaCuenta

--- src/main/java/com/mycompany/ud4_te1/Main.java
+++ src/main/java/com/mycompany/ud4_te1/Main.java
@@ -7,10 +7,15 @@ public class Main {
         CCuenta cuentaTrabajo;
         double saldoActual;
 
+        operativaCuenta();
+    }
+
+    private static void operativaCuenta() {
+        CCuenta cuentaTrabajo;
+        double saldoActual;
         cuentaTrabajo = new CCuenta("Antonio López","1000-2365-85-1230456789", 2500,0);
         saldoActual = cuentaTrabajo.estado();
         System.out.println("El saldo actual es " + saldoActual);
-
         try {
             System.out.println("Cargo en cuenta");
             cuentaTrabajo.retirar(2300);
@@ -18,7 +23,6 @@ public class Main {
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
-        
         try {
             System.out.println("Abono en cuenta");
             cuentaTrabajo.ingresar(695);
