ef4c4dc82200f517e7dc61de807174d7ff6643bd
Mikel <Mikel@DESKTOP-VF0LKT2>
2023-05-13 19:25:29 +0200

    Version 4 creamos variable cantidad y la pasamos a operativaCuenta

--- src/main/java/com/mycompany/ud4_te1/Main.java
+++ src/main/java/com/mycompany/ud4_te1/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuentaTrabajo;
         double saldoActual;
-
-        operativaCuenta();
+        double cantidad = 2300;
+        operativaCuenta(cantidad);
     }
 
-    private static void operativaCuenta() {
+    private static void operativaCuenta(double cantidad) {
         CCuenta cuentaTrabajo;
         double saldoActual;
         cuentaTrabajo = new CCuenta("Antonio López","1000-2365-85-1230456789", 2500,0);
@@ -18,7 +18,7 @@ public class Main {
         System.out.println("El saldo actual es " + saldoActual);
         try {
             System.out.println("Cargo en cuenta");
-            cuentaTrabajo.retirar(2300);
+            cuentaTrabajo.retirar(cantidad);
             System.out.println("El nuevo saldo es: " + cuentaTrabajo.saldo());
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
